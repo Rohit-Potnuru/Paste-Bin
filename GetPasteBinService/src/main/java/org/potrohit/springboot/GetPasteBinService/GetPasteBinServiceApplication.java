95adad70eec9b522f70527e8437ba7f845529e0a
Rohit Potnuru <rohitpotnuru1997@gmail.com>
2023-09-02 16:15:07 -0500

    Creating PasteBin Application

---
+++ GetPasteBinService/src/main/java/org/potrohit/springboot/GetPasteBinService/GetPasteBinServiceApplication.java
@@ -0,0 +1,13 @@
+package org.potrohit.springboot.GetPasteBinService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetPasteBinServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GetPasteBinServiceApplication.class, args);
+    }
+}
+
+
